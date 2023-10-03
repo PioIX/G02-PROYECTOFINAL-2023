dbd5936449769c799e0fc704cb110fe4828a0ac0
jpbertuglia <suletajuanignacio@pioix.edu.ar>
2023-10-03 10:02:13 -0300

    login

--- index.js
+++ index.js
@@ -78,3 +78,93 @@ app.delete('/login', function(req, res) {
     console.log("Soy un pedido DELETE", req.body); //En req.body vamos a obtener el objeto con los parámetros enviados desde el frontend por método DELETE
     res.send(null);
 });
+
+id_logeado = -1
+app.get('/', function(req, res)
+{
+    //Petición GET con URL = "/", lease, página principal.
+    console.log(req.query); //En req.query vamos a obtener el objeto con los parámetros enviados desde el frontend por método GET
+    res.render('login', null); //Renderizo página "login" sin pasar ningún objeto a Handlebars
+});
+
+app.get('/login', function(req, res)
+{
+    //Petición GET con URL = "/login"
+    console.log("Soy un pedido GET", req.query); 
+    //En req.query vamos a obtener el objeto con los parámetros enviados desde el frontend por método GET
+    res.render('home', null); //Renderizo página "home" sin pasar ningún objeto a Handlebars
+});
+app.get('/register', function(req, res)
+{
+    console.log("Soy un pedido GET", req.query)
+    res.render('register', null)
+}
+)
+app.post('/register', async function(req, res)
+{
+    console.log("Soy un pedido POST", req.body)
+    
+    await MySQL.realizarQuery(`INSERT INTO usuarios (dni,nombre_usuario, nombre_completo, contraseña_usuario, usuario_admin, puntaje) VALUES("${req.body.dni}","${req.body.Usuario}", "${req.body.Name}", "${req.body.Contraseña}", ${false}, ${0})`)
+    
+    res.render('login', null)
+}
+)
+
+app.post('/login', async function(req, res)
+{
+    //Petición POST con URL = "/login"
+    console.log("Soy un pedido POST", req.body); 
+    //En req.body vamos a obtener el objeto con los parámetros enviados desde el frontend por método POST
+    //res.render('home', { mensaje: "Hola mundo!", usuario: req.body.usuario}); //Renderizo página "home" enviando un objeto de 2 parámetros a Handlebars
+     //Renderizo página "home" sin pasar ningún objeto a Handlebars
+    let palabras = await MySQL.realizarQuery("SELECT * FROM palabras")
+    let usuarios = await MySQL.realizarQuery("SELECT * FROM usuarios")
+    let verificar = 0
+    for (i in usuarios) {
+        if (usuarios[i].nombre_usuario == req.body.usuario) {
+            if (usuarios[i].contraseña_usuario == req.body.contraseña) {
+                verificar = 1
+                id_logeado = usuarios[i].id_usuario
+                console.log(id_logeado)
+                if (usuarios[i].usuario_admin == true) {
+                    verificar = 2
+                    id_logeado = usuarios[i].id_usuario
+                }
+            }
+        }
+    } 
+        
+    console.log(verificar)
+    if (verificar == 0) {
+        console.log("no existe user o no es valida contraseña")
+        res.render('login', null)
+    }
+    else if(verificar == 1) {
+        console.log("logeado lv user normal")
+        console.log(id_logeado)
+        res.render('home', {users: usuarios, words: palabras})
+    }
+    else if (verificar == 2) {
+        console.log("logeado admin")
+      //  res.render('admin', {users: usuarios, words: palabras})
+      res.redirect("/renAdmin")
+    }
+
+    });
+
+app.put('/login', async function(req, res) {
+    //Petición PUT con URL = "/login"
+    console.log("Soy un pedido PUT", req.body); //En req.body vamos a obtener el objeto con los parámetros enviados desde el frontend por método PUT
+    //Consulto en la bdd de la existencia del usuario
+    let respuesta = await MySQL.realizarQuery(`SELECT * FROM usuarios WHERE nombre_usuario = "${req.body.user}" AND contraseña_usuario = "${req.body.pass}"`)
+    //Chequeo el largo del vector a ver si tiene datos
+    if (respuesta.length > 0) {
+        //Armo un objeto para responder
+        res.send({validar: true})    
+    }
+    else{
+        res.send({validar:false})    
+    }
+    
+    
+});
---
+++ public/js/dom.js
@@ -0,0 +1,60 @@
+function changeScreen() {
+    const notepad = document.getElementById("notepad");
+    const login = document.getElementById("login");
+    if(notepad.style.display !== "none") {
+        notepad.style.display = "none";
+        login.style.display = "";
+    }
+    else {
+        notepad.style.display = "";
+        login.style.display = "none";
+    }
+  }
+  
+  function getSearchID() {
+    return document.getElementById("searchByID").value;
+  }
+  
+  
+  async function putJSON(data) {
+    //putJSON() es solo el nombre de esta funcion que lo pueden cambiar    
+  
+try {
+    const response = await fetch("/login", {
+      method: "PUT", // or 'POST'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    
+    //En result obtengo la respuesta
+    const result = await response.json();
+    console.log("Success:", result);
+
+    if (result.validar == false) {
+      alert("Los datos son incorrectos, intente ingresando devuelta o registrandose")
+    } else {
+      //Envio el formularia desde dom para cambiar de pagina
+      //Podria usar tambien un changeScreen()
+      document.getElementById("form1").submit()
+    }
+
+  } catch (error) {
+    console.error("Error:", error);
+    }
+}
+
+function login() {
+    //Leo los datos del input
+    let usuario = document.getElementById("usuarioId").value
+    let contraseña = document.getElementById("passwordId").value
+  
+    //Creo un objeto de forma instantanea
+    let data = {
+        user: usuario,
+        pass: contraseña  
+    }
+    putJSON(data)  
+}
+  
