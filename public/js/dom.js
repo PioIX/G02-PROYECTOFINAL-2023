ec9cb6ec397e3de740914d4982467d2f648415bd
santinoamadey <129756395+santinoamadey@users.noreply.github.com>
2023-10-31 09:33:20 -0300

    Update dom.js

---
+++ public/js/dom.js
@@ -0,0 +1,17 @@
+<<<<<<< Updated upstream
+=======
+let casillero = 1
+
+
+function tirarDado(){
+    let valor = Math.floor(Math.random() * 6)+1
+    console.log(valor)
+    casillero += valor
+    if (casillero > 39){
+        casillero = casillero % 39
+        console.log(casillero)
+    }
+
+}
+
+>>>>>>> Stashed changes
