eff0b949b74a3e58163ca98a372acfad29028bab
FelipeVegaTorre <101184306+FelipeVegaTorre@users.noreply.github.com>
2023-11-06 10:23:08 -0300

    Update dom.js

--- public/js/dom.js
+++ public/js/dom.js
@@ -5,8 +5,8 @@ function tirarDado(){
     let valor = Math.floor(Math.random() * 6)+1
     console.log(valor)
     casillero += valor
-    if (casillero > 39){
-        casillero = casillero % 39
+    if (casillero > 44){
+        casillero = casillero % 44
         console.log(casillero)
     }
 
@@ -17,12 +17,33 @@ function posicionjugador(){
 
 }
 
-function avanzarPosicion(){
+
+function chequearPregunta(){
+    let tipoPregunta = ""
+    let color_casillero = document.getElementById().style.backgroundColor()
+    if (color_casillero = 1 ){
+        tipoPregunta = "historia"
+    }
+    else if (color_casillero = 1 ){
+        tipoPregunta = "deporte"
+    }
+    else if (color_casillero = 1 ){
+        tipoPregunta = "arte"
+    }
+    else if (color_casillero = 1 ){
+        tipoPregunta = "ciencia"
+    }
+    else if (color_casillero = 1 ){
+        tipoPregunta = "entretenimiento"
+    }
+    else {
+        tipoPregunta = "entretenimiento"
+    }
 
 
-}
+    let data = {
+        pregunta : tipoPregunta
+    }
+    chequearPreguntaWS(data)
+};
 
-function retrocederPosicion(){
-
-
-}
