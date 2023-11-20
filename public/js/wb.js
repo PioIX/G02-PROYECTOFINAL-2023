31ccdbf86c1425b232025afc8364a2ab8069ceb5
sloprete-pioix <129756032+sloprete-pioix@users.noreply.github.com>
2023-11-20 16:01:04 -0300

    Update wb.js

--- public/js/wb.js
+++ public/js/wb.js
@@ -1,4 +1,4 @@
-const IP = "ws://localhost:3000";
+const IP = "ws://localhost:3002";
 const socket = io(IP);
 
 
@@ -17,18 +17,197 @@ function funcionPrueba() {
 
 
 function chequearPreguntaWS(data){
+    socket.emit("tipo-pregunta", data);
+    console.log(data.pregunta)
+}
+
+socket.on('mandar-pregunta', data => {
+    
+    console.log("pregunta", data);        
+});
+
+function unirseSala() {
+    console.log(sessionStorage.getItem('sala'))
+    socket.emit("unirse-sala", {rooms: sessionStorage.getItem('sala')});
+}
+
+function crearSala() {
+  socket.emit("crear-sala", {rooms: sessionStorage.getItem('sala')});
+}
+
+
+function conseguirSala() {
+    sala = document.getElementById("Sala").value
+    crearSala(sala)
+}
+
+async function unirseRoom(dataJoinRoom) {
+    //putJSON() es solo el nombre de esta funcion que lo pueden cambiar    
+  
+    try {
+      const response = await fetch("/joinGame", {
+        method: "PUT", // or 'POST'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataJoinRoom),
+      });
+      
+      //En result obtengo la respuesta
+      const result = await response.json();
+      console.log("Success:", result);
+  
+      if (result.validar == false) {
+        alert("La sala a la que intenta unirse no existe o esta llena")
+      } else {
+        //Envio el formularia desde dom para cambiar de pagina
+        //Podria usar tambien un changeScreen()
+        let sala2= document.getElementById("Sala2").value
+        location.href = '/game'
+        
+        
+      }
+  
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  
+  function joinRoom() {
+    //Leo los datos del input
+    let sala2= document.getElementById("Sala2").value
+    //Creo un objeto de forma instantanea
+    let dataJoinRoom= {
+        sala: sala2
+    }
+    sessionStorage.setItem('sala', sala2)
+    unirseRoom(dataJoinRoom)
+  }
+
+
+  async function createRoom(dataCreateRoom) {
+    //putJSON() es solo el nombre de esta funcion que lo pueden cambiar    
+  
+    try {
+      const response = await fetch("/createRoom", {
+        method: "POST", // or 'POST'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataCreateRoom),
+      });
+      
+      //En result obtengo la respuesta
+      const result = await response.json();
+      console.log("Success:", result);
+  
+      if (result.validar == false) {
+        alert("La sala que intenta crear ya existe")
+      } else {
+        //Envio el formularia desde dom para cambiar de pagina
+        //Podria usar tambien un changeScreen()
+        let sala = document.getElementById("Sala").value
+        location.href = '/game'
+        
+        
+        
+      }
+  
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  
+  function createRoom2() {
+    //Leo los datos del input
+    let sala1= document.getElementById("Sala").value
+    //Creo un objeto de forma instantanea
+    let dataCreateRoom= {
+        sala: sala1
+    }
+    sessionStorage.setItem('sala', sala1)
+    createRoom(dataCreateRoom)
+  }
+
+  async function golazo(envido) {
+    //putJSON() es solo el nombre de esta funcion que lo pueden cambiar    
+  
+    try {
+      const response = await fetch("/changeScreen", {
+        method: "POST", // or 'POST'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(envido),
+      });
+      
+      //En result obtengo la respuesta
+      const result = await response.json();
+      console.log("Success:", result);
+  
+      if (result.validar == false) {
+        alert("caca")
+      } else {
+        //Envio el formularia desde dom para cambiar de pagina
+        //Podria usar tambien un changeScreen()
+        location.href = '/game'
+        
+        
+      }
+  
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  
+  function noMeLoCreo() {
+    //Leo los datos del input
+    let hola = "hola"
+    //Creo un objeto de forma instantanea
+    let envido = {
+        hola1: hola
+    }
+    golazo(envido)
+  }
+
+socket.on("conexion-user", data => {
+  console.log("SE UNIO USUARIO CON ID: ", data.user)
+});
+socket.on("conexion-user", data => {
+  console.log("SE UNIO USUARIO CON ID: ", data.user)
+});
+
+socket.on("conexion-user", data => {
+  console.log("SE UNIO USUARIO CON ID: ", data.user)
+});
+
+
+function changeWaitingRoom() {
+  socket.emit('iniciar-partida', sessionStorage.getItem('sala'))
+  let espera = document.getElementById("espera")
+  let juego = document.getElementById("juego")
+
+  if(espera.style.display !="none") {
+    espera.style.display = "none"
+    juego.style.display = ""
+  }  
+}
+function chequearPreguntaWS(data){
   console.log(data.pregunta)
     socket.emit("tipo-pregunta", data);
 }
 
+
 let respuesta_correcta = "opcion"
+
 
 socket.on('mandar-pregunta', data => {
   // myModal.show()
-   document.getElementById("opcion0").style.backgroundColor = "#6c757d" 
-   document.getElementById("opcion1").style.backgroundColor = "#6c757d" 
-   document.getElementById("opcion2").style.backgroundColor = "#6c757d" 
-   document.getElementById("opcion3").style.backgroundColor = "#6c757d" 
+  cronometro()
+   document.getElementById("opcion0").style.backgroundColor = "#6c757d"
+   document.getElementById("opcion1").style.backgroundColor = "#6c757d"
+   document.getElementById("opcion2").style.backgroundColor = "#6c757d"
+   document.getElementById("opcion3").style.backgroundColor = "#6c757d"
    console.log(data.opciones)
    respuesta_correcta = data.opciones
    document.getElementById("id_titulo").innerHTML = data.pregunta.content
@@ -44,170 +223,6 @@ socket.on('mandar-pregunta', data => {
      }
  });
 
-function unirseSala(sala_user) {
-  socket.emit("unirse-sala", {sala: sala_user});
-}
-
-function crearSala(sala_user) {
-    console.log(sala_user)
-    socket.emit("crear-sala", {sala: sala_user,id: socket.id});
-}
-
-
-function conseguirSala() {
-    sala = document.getElementById("Sala").value
-    crearSala(sala)
-}
-
-function unirseSala2() {
-    sala = document.getElementById("Sala").value
-    unirseSala(sala_user)
-}
-
-async function unirseRoom(dataJoinRoom) {
-  //putJSON() es solo el nombre de esta funcion que lo pueden cambiar    
-
-  try {
-    const response = await fetch("/joinGame", {
-      method: "PUT", // or 'POST'
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(dataJoinRoom),
-    });
-    
-    //En result obtengo la respuesta
-    const result = await response.json();
-    console.log("Success:", result);
-
-    if (result.validar == false) {
-      alert("La sala a la que intenta unirse no existe o esta llena")
-    } else {
-      //Envio el formularia desde dom para cambiar de pagina
-      //Podria usar tambien un changeScreen()
-      let sala2= document.getElementById("Sala2").value
-      location.href = '/waitingRoom'
-      unirseSala(sala2)
-      
-      
-    }
-
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
-function joinRoom() {
-  //Leo los datos del input
-  let sala2= document.getElementById("Sala2").value
-  //Creo un objeto de forma instantanea
-  let dataJoinRoom= {
-      sala: sala2
-  }
-  unirseRoom(dataJoinRoom)
-}
-
-
-async function createRoom(dataCreateRoom) {
-  //putJSON() es solo el nombre de esta funcion que lo pueden cambiar    
-
-  try {
-    const response = await fetch("/createRoom", {
-      method: "POST", // or 'POST'
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(dataCreateRoom),
-    });
-    
-    //En result obtengo la respuesta
-    const result = await response.json();
-    console.log("Success:", result);
-
-    if (result.validar == false) {
-      alert("La sala que intenta crear ya existe")
-    } else {
-      //Envio el formularia desde dom para cambiar de pagina
-      //Podria usar tambien un changeScreen()
-      let sala = document.getElementById("Sala").value
-      location.href = '/waitingRoom'
-      crearSala(sala)
-      
-      
-    }
-
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
-function createRoom2() {
-  //Leo los datos del input
-  let sala1= document.getElementById("Sala").value
-  //Creo un objeto de forma instantanea
-  let dataCreateRoom= {
-      sala: sala1
-  }
-  createRoom(dataCreateRoom)
-}
-
-async function golazo(envido) {
-  //putJSON() es solo el nombre de esta funcion que lo pueden cambiar    
-
-  try {
-    const response = await fetch("/changeScreen", {
-      method: "POST", // or 'POST'
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(envido),
-    });
-    
-    //En result obtengo la respuesta
-    const result = await response.json();
-    console.log("Success:", result);
-
-    if (result.validar == false) {
-      alert("caca")
-    } else {
-      //Envio el formularia desde dom para cambiar de pagina
-      //Podria usar tambien un changeScreen()
-      location.href = '/game'
-      
-      
-    }
-
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
-function noMeLoCreo() {
-  //Leo los datos del input
-  let hola = "hola"
-  //Creo un objeto de forma instantanea
-  let envido = {
-      hola1: hola
-  }
-  golazo(envido)
-}
-
-socket.on("conexion-user", data => {
-  console.log("SE UNIO USUARIO CON ID: ", data.user)
-});
-
-
-
-function changeWaitingRoom() {
-  socket.emit('iniciar-partida', sessionStorage.getItem('sala'))
-  let espera = document.getElementById("espera")
-  let juego = document.getElementById("juego")
-
-  if(espera.style.display !="none") {
-    espera.style.display = "none"
-    juego.style.display = ""
-  }  
-}
 
 socket.on("empieza-partida", data => {
   let espera = document.getElementById("espera")
@@ -230,7 +245,5 @@ socket.on("empieza-partida", data => {
 )
 
 socket.on("actualizar", data => {
-  console.log("hola")
-  document.getElementById("usersInRoom").innerHTML =  "Usuarios conectados: " + data.users
+    document.getElementById("usersInRoom").innerHTML =  "Usuarios conectados: " + data.users
 })
-
